8ebc2f6d85dde835142ff04f75dc2bf11310edd3
qidasheng2012 <qidasheng2012@163.com>
2019-09-23 18:21:42 +0800

    add  @ApiImplicitParams

--- src/main/java/com/example/swagger2/config/Swagger2Config.java
+++ src/main/java/com/example/swagger2/config/Swagger2Config.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,21 +14,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.swagger2"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.swagger2")) //扫描路径
+                .paths(PathSelectors.any()) //定义哪些路径的接口需要生成文档
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger2 接口文档")
-                .description("接口文档")
-                .version("1.0")
+                .title("Swagger2 接口文档") //文档首页标题
+                .description("接口文档") //文档描述信息
+                .contact(new Contact("name", "url", "email")) //创建者信息
+                .version("1.0") //文档版本
                 .build();
     }
 }
--- src/main/java/com/example/swagger2/controller/UserController.java
+++ src/main/java/com/example/swagger2/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.example.swagger2.controller;
 
 import com.example.swagger2.model.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
 @RestController
@@ -15,10 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "用户 Controller")
 public class UserController {
 
+    // 创建用户
+    @ApiIgnore // 忽略，不生成文档
     @PostMapping
-    @ApiOperation(value = "创建用户")
     public User create(@RequestBody User user) {
         return user;
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "根据ID获取用户信息", notes = "根据ID获取用户信息详细描述")
+    public User getUser(@PathVariable("id") Long id) {
+        return new User(1L, "张三", 23);
+    }
+
+    @ApiOperation(value = "删除用户", notes = "删除用户详细描述")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户的唯一标识", required = true),
+            @ApiImplicitParam(name = "name", value = "用户名", required = false)
+    })
+    @ApiResponses({
+            @ApiResponse(code = 401, message = "禁止访问")
+    })
+    @DeleteMapping("/{id}/{name}")
+    public String deleteUsers(@PathVariable("id") Long id, @PathVariable("name") String name) {
+        return "删除成功";
     }
 
 }
